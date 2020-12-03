6dcb4ec4717e2d2ab3ffe84cd3f25b85248a8a00
Giordon Stark <kratsg@gmail.com>
2020-12-03 13:10:42 -0500

    get the app working for getting access token

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 from instagram_basic_display.InstagramBasicDisplay import InstagramBasicDisplay
 import os
 from flask import Flask, request
+import webbrowser
 
 app_id = os.getenv('IG_APP_ID')
 app_secret = os.getenv('IG_APP_SECRET')
@@ -8,7 +9,9 @@ app_url = os.getenv('IG_APP_URL')
 
 instagram_basic_display = InstagramBasicDisplay(app_id=app_id, app_secret=app_secret, redirect_url=app_url)
 
-print(instagram_basic_display.get_login_url()) # Returns login URL you need to follow
+login_url = instagram_basic_display.get_login_url() # Returns login URL you need to follow
+print(login_url)
+webbrowser.open(login_url, new=2)
 
 app = Flask(__name__)
 @app.route('/')
@@ -22,7 +25,9 @@ def redirect_url():
     # Exchange this token for a long lived token (valid for 60 days)
     long_lived_token = instagram_basic_display.get_long_lived_token(short_lived_token.get('access_token'))
 
-    print('Your token is: {}' .format(long_lived_token.get('access_token')))
+    output = f"Your token is: {long_lived_token.get('access_token')}"
+    print(output)
+    return output
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc', host='0.0.0.0', port=3000)
